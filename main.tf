96545394968a24bb934007dd79be965884407ea4
Ali I Samji <aisamji09@gmail.com>
2019-08-22 08:42:10 -0500

    Added user and SSH key resources.

--- main.tf
+++ main.tf
@@ -14,5 +14,32 @@ data "aws_iam_policy_document" "default" {
 resource "aws_iam_role" "default" {
   name               = "${var.user_name}"
   assume_role_policy = "${data.aws_iam_policy_document.default.json}"
-  tags               = "${var.tags}"
+
+  tags = "${var.tags}"
+}
+
+# AWS Transfer Server User
+
+resource "aws_transfer_user" "default" {
+  server_id = "${var.server_id}"
+  user_name = "${var.user_name}"
+  role      = "${aws_iam_role.default.arn}"
+
+  tags = "${var.tags}"
+}
+
+# SSH Keys
+
+resource "aws_transfer_ssh_key" "primary" {
+  server_id = "${var.server_id}"
+  user_name = "${var.user_name}"
+  body      = "${var.ssh_keys[0]}"
+}
+
+resource "aws_transfer_ssh_key" "additional" {
+  count = "${length(var.ssh_keys) - 1}"
+
+  server_id = "${var.server_id}"
+  user_name = "${var.user_name}"
+  body      = "${element(var.ssh_keys, count.index + 1)}"
 }
