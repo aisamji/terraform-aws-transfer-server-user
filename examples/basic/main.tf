eeff312dbe08e0cd0b663a2ba40068e7c66b92a0
Ali I Samji <aisamji09@gmail.com>
2019-08-22 12:20:06 -0500

    Fix basic example.

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -10,3 +10,11 @@ module "server_user" {
   bucket_name = "${var.bucket_name}"
   ssh_keys    = "${var.ssh_keys}"
 }
+
+# While this policy allows full access to all S3 buckets,
+# the server_user has a scope-down policy that restricts its access to a
+# specific prefix in a single bucket.
+resource "aws_iam_role_policy_attachment" "full_access" {
+  role       = "${module.server_user.role_name}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
